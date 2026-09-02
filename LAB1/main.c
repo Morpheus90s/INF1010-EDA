657a0463e3a9616eeada3b05e47a740a023cb6ee
Pedro Augusto <100485541+Morpheus90s@users.noreply.github.com>
2026-09-02 17:09:24 +0000

    alteracoes

--- LAB1/main.c
+++ LAB1/main.c
@@ -6,39 +6,62 @@
 int main(void) {
     char tipo;
     int valor;
-
     FILE* arq_f = fopen("entrada_fila.txt", "r");
-    if (arq_f != NULL) {
-        Fila* f = fila_cria();
-        while (fscanf(arq_f, " %c", &tipo) != EOF) {
+    
+    if (arq_f != NULL) { 
+        Fila* lista = fila_cria(); 
+        
+    
+        while (fscanf(arq_f, "%c", &tipo) != EOF) {
+            
             if (tipo == 'a') {
-                fscanf(arq_f, "%d", &valor);
-                ARMAZENA(f, valor);
-                fila_imprime(f);
-            } else if (tipo == 'r') {
-                RETIRA(f);
-                fila_imprime(f);
+                fscanf(arq_f, "%d", &valor); 
+                ARMAZENA(lista, valor);
+                fila_imprime(lista);
+            } 
+            else if (tipo == 'r') {
+                RETIRA(lista);
+                fila_imprime(lista);
+            } 
+            else if (tipo == '\n') {
+                printf("----------\n");
+                fila_libera(lista);      
+                lista = fila_cria();    
             }
         }
-        fclose(arq_f); 
-        fila_libera(f);
+        
+        fclose(arq_f);      
+        fila_libera(lista);   
     }
 
+    printf("Pilha\n")
     FILE* arq_p = fopen("entrada_pilha.txt", "r");
+    
     if (arq_p != NULL) {
         Pilha* p = pilha_cria();
-        while (fscanf(arq_p, " %c", &tipo) != EOF) {
+        
+        // Radar sensível idêntico ao da fila para detectar o \n
+        while (fscanf(arq_p, "%c", &tipo) != EOF) {
+            
             if (tipo == 'e') {
                 fscanf(arq_p, "%d", &valor);
                 PUSH(p, valor);
                 pilha_imprime(p);
-            } else if (tipo == 'r') {
+            } 
+            else if (tipo == 'r') {
                 POP(p);
                 pilha_imprime(p);
             }
+            else if (tipo == '\n') {
+                printf("----------\n");
+                pilha_libera(p);     
+                p = pilha_cria();   
+            }
         }
-        fclose(arq_p); 
+        
+        fclose(arq_p);
         pilha_libera(p);
     }
+    
     return 0;
 }
